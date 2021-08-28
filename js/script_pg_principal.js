967e6b5f0731e720ea2e6605d5c59f87043495bd
edgar34c <edgararaujocorrea@gmail.com>
2021-08-28 14:12:43 -0300

    Ajustes na página de login e adicionado comentarios no codigo

--- js/script_pg_principal.js
+++ js/script_pg_principal.js
@@ -2,8 +2,9 @@
 var sair  = document.querySelector("#sair");
 var pedidos = document.querySelector("#pedidos");
 var estoque = document.querySelector("#estoque");
-var tbl_pedidos = document.querySelector("#tbl_pedidos")
-var tbl_estoque = document.querySelector("#tbl_estoque")
+var tbl_pedidos = document.querySelector("#tbl_pedidos");
+var tbl_estoque = document.querySelector("#tbl_estoque");
+var body = document.getElementsByTagName("body");
 
 //volta para pagina de login
 sair.addEventListener("click", function(e){
@@ -15,7 +16,7 @@ sair.addEventListener("click", function(e){
 pedidos.addEventListener("click", function(e){
     e.preventDefault("");
     if(tbl_estoque.style.display === "block"){
-        tbl_estoque.style.display = "none"
+        tbl_estoque.style.display = "none";
     }
     tbl_pedidos.style.display = "block";
 });
@@ -24,8 +25,7 @@ pedidos.addEventListener("click", function(e){
 estoque.addEventListener("click", function(e){
     e.preventDefault();
     if(tbl_pedidos.style.display === "block"){
-        tbl_pedidos.style.display = "none"
+        tbl_pedidos.style.display = "none";
     }    
     tbl_estoque.style.display = "block";
-
 });
